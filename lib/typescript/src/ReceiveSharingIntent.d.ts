bc4da4b9ace995b3476385761d0ec109b8ab25ed
Derek Chan <derekchan916@gmail.com>
2025-05-08 13:49:50 +0800

    Upgrade yarn

--- lib/typescript/src/ReceiveSharingIntent.d.ts
+++ lib/typescript/src/ReceiveSharingIntent.d.ts
@@ -1,8 +1,12 @@
-import type { IReceiveSharingIntent } from "./ReceiveSharingIntent.interfaces";
+import type { IReceiveSharingIntent } from './ReceiveSharingIntent.interfaces';
+/**
+ * Documentation: Please read receivingSharing.ts in breakroom-mobile.
+ * Original documentation here was supposed to be from https://github.com/ajith-ab/react-native-receive-sharing-intent/blob/master/src/ReceiveSharingIntent.ts
+ * but it was not working for our case.
+ */
 declare class ReceiveSharingIntentModule implements IReceiveSharingIntent {
     private isIos;
     private utils;
-    private isClear;
     getReceivedFiles(handler: Function, errorHandler: Function, protocol?: string): void;
     clearReceivedFiles(): void;
     protected getFileNames(handler: Function, errorHandler: Function, url: string): void;
